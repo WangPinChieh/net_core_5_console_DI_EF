29238ac4d8cc4f32c706f9fa1daa5bbc892f8119
Jay Wang <jay.wang@hp.com>
2021-11-17 16:37:21 +0800

    add UpdatedDate

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,15 +1,13 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ConsoleApp1.Models
 {
     public class Student
     {
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public Guid ID { get; set; } = Guid.NewGuid();
+        public Guid ID { get; set; } 
         public string LastName { get; set; }
         public string FirstMidName { get; set; }
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime UpdatedDate { get; set; } = DateTime.Now;
     }
 }
